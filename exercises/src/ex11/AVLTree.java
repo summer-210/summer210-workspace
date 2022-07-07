571158051e7541813af77584e0f9f0f4cf7bc3ac
Kaki Ryan <kakiryan@cs.unc.edu>
2022-07-07 13:26:50 -0400

    ex11

---
+++ exercises/src/ex11/AVLTree.java
@@ -0,0 +1,98 @@
+package ex11;
+
+public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
+    // Fields
+    private T _value;
+    private AVLTree<T> _left;
+    private AVLTree<T> _right;
+    private int _height;
+    private int _size;
+    
+    public AVLTree() {
+        _value = null;
+        _left = null;
+        _right = null;
+        _height = -1;
+        _size = 0;
+    }
+
+    /**
+     *
+     * Rotates the tree left and returns
+     * AVLTree root for rotated result.
+     */
+    
+     private AVLTree<T> rotateLeft() {
+         // You should implement left rotation and then use this 
+         // method as needed when fixing imbalances.
+         return null;
+     }
+    
+    /**
+     *
+     * Rotates the tree right and returns
+     * AVLTree root for rotated result.
+     */ 
+     
+     private AVLTree<T> rotateRight() {
+         // You should implement right rotation and then use this 
+         // method as needed when fixing imbalances.
+         return null;
+     }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public int height() {
+        return _height;
+    }
+
+    @Override
+    public int size() {
+        return _size;
+    }
+
+    @Override
+    public SelfBalancingBST<T> insert(T element) {
+        return null;
+    }
+
+    @Override
+    public SelfBalancingBST<T> remove(T element) {
+        return null;
+    }
+
+    @Override
+    public T findMin() {
+        return null;
+    }
+
+    @Override
+    public T findMax() {
+        return null;
+    }
+
+    @Override
+    public boolean contains(T element) {
+        return false;
+    }
+
+    @Override
+    public T getValue() {
+        return _value;
+    }
+
+    @Override
+    public SelfBalancingBST<T> getLeft() {
+        return _left;
+    }
+
+    @Override
+    public SelfBalancingBST<T> getRight() {
+        return _right;
+    }
+
+}
